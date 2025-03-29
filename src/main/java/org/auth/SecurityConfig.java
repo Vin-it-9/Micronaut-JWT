3e1eb09f55bb9c06cbabca313458daae0fb1c36c
Vin-it-9 <vineetshinde2559@gmail.com>
2025-03-29 20:42:11 +0530

    updated with security

--- src/main/java/org/auth/SecurityConfig.java
+++ src/main/java/org/auth/SecurityConfig.java
@@ -9,17 +9,15 @@ import jakarta.inject.Singleton;
 @Factory
 public class SecurityConfig {
 
-    private static final String DEFAULT_SECRET = "4fP7jL$Kx8!dVb@tQy9&eZsG3*mRwC6^hA2#nW5fUvS%qE1zXcB0pT_iY+aD}oM{NuJr-Hk=gI|l2O.F,vZ/xL;wK~pQm?j4R>d7U<6tG8sCb3n5aE9hWy!@#$%^&*()VfXzSqNpMgJkHrTeDyLvBcZoAi2U1w";
-
     @Singleton
     @Primary
-    @Named("generator")
     @Replaces(SecretSignatureConfiguration.class)
     public SecretSignatureConfiguration secretSignatureConfiguration() {
-
-        SecretSignatureConfiguration configuration = new SecretSignatureConfiguration(DEFAULT_SECRET);
-        configuration.setJwsAlgorithm(JWSAlgorithm.HS256);
-
-        return configuration;
+        String secret = System.getenv("JWT_SECRET");
+        return new SecretSignatureConfiguration(
+                secret != null ? secret : "4fP7jL$Kx8!dVb@tQy9&eZsG3*mRwC6^hA2#nW5fUvS%qE1zXcB0pT_iY+aD}oM{NuJr-Hk=gI|l2O.F,vZ/xL;wK~pQm?j4R>d7U<6tG8sCb3n5aE9hWy!@#$%^&*()VfXzSqNpMgJkHrTeDyLvBcZoAi2U1w"
+        );
     }
+
+
 }
