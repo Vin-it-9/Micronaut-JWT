afe9bb0668aa349d8e10f089ee92098f6fab7ae3
Vin-it-9 <vineetshinde2559@gmail.com>
2025-03-29 20:34:05 +0530

    updated with security

--- src/main/java/org/auth/SecurityConfig.java
+++ src/main/java/org/auth/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.auth;
 
 import com.nimbusds.jose.JWSAlgorithm;
 import io.micronaut.context.annotation.*;
-import io.micronaut.core.annotation.Nullable;
 import io.micronaut.security.token.jwt.signature.secret.SecretSignatureConfiguration;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
@@ -10,7 +9,7 @@ import jakarta.inject.Singleton;
 @Factory
 public class SecurityConfig {
 
-    private static final String DEFAULT_SECRET = "aB3#F8eZ!2jL0sV9mX1pQ6rT5wN7uK4dY2zC8gH3oJ0lR5iM1nU4vW7xE9yP0Z#1";
+    private static final String DEFAULT_SECRET = "4fP7jL$Kx8!dVb@tQy9&eZsG3*mRwC6^hA2#nW5fUvS%qE1zXcB0pT_iY+aD}oM{NuJr-Hk=gI|l2O.F,vZ/xL;wK~pQm?j4R>d7U<6tG8sCb3n5aE9hWy!@#$%^&*()VfXzSqNpMgJkHrTeDyLvBcZoAi2U1w";
 
     @Singleton
     @Primary
